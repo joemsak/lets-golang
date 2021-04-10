cabf2bbd4bd5be770c6a5c5647c53f0e509bc38f
Joe Sak <joe@joesak.com>
2021-04-10 13:22:29 -0500

    Finish first set of examples

--- code/snippetbox/main.go
+++ code/snippetbox/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	//
+	// First call to Write() automatically sends 200 OK status
+	//
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//
+		// Header().Set() must be called BEFORE WriteHeader() && Write()
+		//
+
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
